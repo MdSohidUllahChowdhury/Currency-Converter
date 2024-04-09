f4c44c5d61080d953c995a0ba23e5cd0736feb49
Sohid Ullah <mdsohidullahchowdhury1@gmail.com>
2024-04-09 20:28:06 +0600

    Working

--- lib/converter.dart
+++ lib/converter.dart
@@ -8,23 +8,40 @@ class Emni extends StatefulWidget {
 }
 
 class _EmniState extends State<Emni> {
+
+  double result = 0;
+     
+  final TextEditingController textEditingController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
+    
+    
     return Scaffold(
       backgroundColor: Colors.blueGrey,
-      appBar: AppBar(
+      appBar:
+       AppBar(
         backgroundColor: Colors.blueGrey,
         title: Text('Currency Converter',
         style: TextStyle(fontSize: 18,color: Colors.white),),
         centerTitle: true,
         elevation: 0,),
+     
       body:
        Column(mainAxisAlignment: MainAxisAlignment.center,
         children: [
           
+          Text(result.toString(),
+          style: TextStyle(
+            fontSize: 60,
+            fontWeight: FontWeight.bold,
+            color: Colors.white),
+            ),
+
           Padding(
             padding: const EdgeInsets.all(8.0),
             child: TextFormField(
+              controller: textEditingController,
               keyboardType: TextInputType.number,
               decoration: InputDecoration(
                 hintText: 'Please enter the amount in USD',
@@ -41,11 +58,16 @@ class _EmniState extends State<Emni> {
           
              SizedBox(height: 10,),
 
-            TextButton(onPressed: null,
+            TextButton(onPressed: (){
+              setState(() {
+                result = double.parse(textEditingController.text)*109.49;
+              });
+            },
             style:ButtonStyle(
               backgroundColor: MaterialStatePropertyAll(Colors.black),
               foregroundColor: MaterialStatePropertyAll(Colors.white),
-              minimumSize: MaterialStatePropertyAll(Size(300,50))
+              //minimumSize: MaterialStatePropertyAll(Size(300,50)),
+              fixedSize: MaterialStatePropertyAll(Size(300, 40))
             ) ,
              child: Text('Convert'))
                 ],
