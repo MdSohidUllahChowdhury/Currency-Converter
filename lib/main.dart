28e1d912387fc94ebc172f104afd5042d166e2d6
Sohid Ullah Chowdhury <mdsohidullahchowdhury1@gmail.com>
2024-10-14 21:03:41 +0600

    Some changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:currency_converter/converter.dart';
+
 void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return ScreenUtilInit(
-     designSize: const Size(360, 640),
-     builder: (context, child)
-         => const MaterialApp(
-             debugShowCheckedModeBanner: false,  
-             home:ConverterScreen()
-             ),
-    );
-  }
+  runApp(ScreenUtilInit(
+    designSize: const Size(360, 640),
+    builder: (context, child) 
+    => const MaterialApp(
+        debugShowCheckedModeBanner: false, 
+        home: ConverterScreen()
+        ),
+  ));
 }
